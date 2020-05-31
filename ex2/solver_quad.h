fdcf2be635e7613644105fda39afd03a83d76359
brennerfjors <rainklimlika123@gmail.com>
2020-05-31 08:50:55 +0300

    Added ex2

---
+++ ex2/solver_quad.h
@@ -0,0 +1,15 @@
+//
+// Created by Rain on 06.05.2020.
+//
+
+#ifndef UNTITLED1_SOLVER_QUAD_H
+#define UNTITLED1_SOLVER_QUAD_H
+#include "solver_base.h"
+
+class solver_quad : public Solver{
+public:
+    virtual void solve_quadro(int aa, int bb, int cc) override;
+};
+
+
+#endif //UNTITLED1_SOLVER_QUAD_H
